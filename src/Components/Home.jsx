15b95c1a5a2fbb92aa06d6a9044c1d6187193db9
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-02-04 22:18:51 +0300

    Added loading and error handling to home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 import Feed from "./Feed";
 import PageFooter from "./PageFooter";
 
-const Home = ({ contacts }) => {
+const Home = ({ contacts, fetchError, isLoading }) => {
   return (
     <main className="Home container">
-      {contacts.length ? 
+      {isLoading && <p>Contacts are loading ...</p>}
+      {!isLoading && fetchError && <p>{fetchError}</p>}
+      {!isLoading && !fetchError && (contacts.length ? 
         <Feed contacts={contacts} /> :
         <p className="text-center my-4 py-4 text-center text-danger">
           There are no contacts available
-        </p>
+        </p>)
       }
       <Link to="/new"><PageFooter className="addButton my-4" /></Link>
     </main>
