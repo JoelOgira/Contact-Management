c9546bece72c960d4b63bbe4bd37f9939126dde1
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-02-05 21:42:34 +0300

    Changed styling

--- src/Components/ContactPage.jsx
+++ src/Components/ContactPage.jsx
@@ -5,7 +5,7 @@ const ContactPage = ({contact}) => {
       <main className="ContactPage">
         <li className="contact-link my-4">
           <Link to={`/contact/${contact.id}`}>
-            <p className="lead contact-p"><span className="contact-initial p-3" >{(contact.name).slice(0,1)}</span> {contact.name}</p>
+            <p className="lead contact-p"><span className="contact-initial py-3 px-3" >{(contact.name).slice(0,1)}</span> {contact.name}</p>
           </Link>
         </li>
       </main>
